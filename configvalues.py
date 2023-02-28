00edda72cae258e22a1d56b29524dde0baf2336e
Jakob Høy <gatlingjakob@hotmail.com>
2023-02-28 19:58:56 +0100

    config values

---
+++ configvalues.py
@@ -0,0 +1,6 @@
+config_values = {
+  "SQLALCHEMY_DATABASE_URI": "sqlite:////Users/jakobhoy/Desktop/Development/Python/musicblog/musicblog.db",
+  "UPLOADED_PHOTOS_DEST": "static/images",
+  "SECRET_KEY": "umamibanana",
+  "USERNAME": "Gatlingjakob"
+}
